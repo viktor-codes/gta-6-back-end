7fde46a2283a58b17dade30d311ffdb404dce50f
Viktor Rura <ruravictor@gmail.com>
2024-07-30 18:16:08 +0100

    Complete the functionality for creating the entity in wait_list table

--- index.ts
+++ index.ts
@@ -1,11 +1,33 @@
 import express from 'express'
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 const app = express()
 const PORT = 5000
 
-app.get('/api', (req, res) => {
-	res.send('Hello World!')
+app.use(express.json())
+
+app.post('/api', async (req, res) => {
+	const { email, name } = req.body
+
+	if (!email || !name)
+		return res.status(400).json({ message: 'Email and name are required' })
+	try {
+		const createdRow = await prisma.waitList.create({
+			data: {
+				email,
+				name,
+			},
+		})
+		res.json(createdRow)
+	} catch (error) {
+		console.log(error)
+		return res.status(500).json({ message: error })
+	}
 })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
 })
