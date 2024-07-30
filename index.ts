f67dd9c4b6c12aa16b7b60c37c3968a69d82c90f
Viktor Rura <ruravictor@gmail.com>
2024-07-30 18:45:50 +0100

    All fixed

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
+import { PrismaClient } from '@prisma/client'
 import cors from 'cors'
 import express from 'express'
 
-import { PrismaClient } from '@prisma/client'
-
 const prisma = new PrismaClient()
-
 const app = express()
 const PORT = 5000
 
@@ -14,8 +12,10 @@ app.use(express.json())
 app.post('/api', async (req, res) => {
 	const { email, name } = req.body
 
-	if (!email || !name)
-		return res.status(400).json({ message: 'Email and name are required' })
+	if (!email || !name) {
+		return res.status(400).json({ message: 'Email and name required fields!' })
+	}
+
 	try {
 		const createdRow = await prisma.waitList.create({
 			data: {
@@ -23,13 +23,13 @@ app.post('/api', async (req, res) => {
 				name,
 			},
 		})
+
 		res.json(createdRow)
 	} catch (error) {
-		console.log(error)
-		return res.status(500).json({ message: error })
+		res.status(400).send({ message: error })
 	}
 })
 
 const server = app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`)
+	console.log(`Listening on PORT ${PORT}`)
 })
